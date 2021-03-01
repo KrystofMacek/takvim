43e551e887d143ac45c547ebdebfe6b219331435
Krystof <krystofmacek333@gmail.com>
2021-03-01 14:41:59 +0100

    compass adj

--- lib/widgets/compass_page/body_content.dart
+++ lib/widgets/compass_page/body_content.dart
@@ -1,8 +1,6 @@
 import 'dart:math' as math;
-import 'package:compasstools/compasstools.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_compass/flutter_compass.dart';
-import 'package:flutter_qiblah/flutter_qiblah.dart';
 
 class CompassBodyContent extends StatefulWidget {
   CompassBodyContent({Key key}) : super(key: key);
@@ -12,66 +10,15 @@ class CompassBodyContent extends StatefulWidget {
 }
 
 class _CompassBodyContentState extends State<CompassBodyContent> {
-  final _deviceSupport = FlutterQiblah.androidDeviceSensorSupport();
   @override
   Widget build(BuildContext context) {
     return Builder(builder: (context) {
-      return FutureBuilder(
-        future: checkDeviceSensors(),
-        builder: (_, AsyncSnapshot<void> snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting)
-            return CircularProgressIndicator();
-          if (snapshot.hasError)
-            return Center(
-              child: Text("Error: ${snapshot.error.toString()}"),
-            );
-
-          // if (snapshot.data) return QiblahCompassWidget();
-          return Container(
-            child: Text('No'),
-          );
-        },
+      return Column(
+        children: <Widget>[
+          Expanded(child: _buildCompass()),
+        ],
       );
     });
-  }
-
-  Future<void> checkDeviceSensors() async {
-    String sensorType;
-    int haveSensor;
-
-    try {
-      haveSensor = await Compasstools.checkSensors;
-      print('haveSensor: $haveSensor');
-
-      switch (haveSensor) {
-        case 0:
-          {
-            sensorType = "No sensors for compass!";
-          }
-          break;
-
-        case 1:
-          {
-            sensorType = "Accelerometer + Magnetometer!";
-          }
-          break;
-
-        case 2:
-          {
-            sensorType = "Gyroscope!";
-          }
-          break;
-
-        default:
-          {
-            sensorType = "Error!";
-          }
-          break;
-      }
-    } on Exception {}
-    if (!mounted) return;
-
-    print(sensorType);
   }
 
   Widget _buildCompass() {
@@ -144,50 +91,6 @@ class _CompassBodyContentState extends State<CompassBodyContent> {
                 ),
               ),
             ),
-          ],
-        );
-      },
-    );
-  }
-}
-
-class QiblahCompassWidget extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return StreamBuilder(
-      stream: FlutterQiblah.qiblahStream,
-      builder: (_, AsyncSnapshot<QiblahDirection> snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting)
-          return CircularProgressIndicator();
-
-        final qiblahDirection = snapshot.data;
-
-        return Stack(
-          alignment: Alignment.center,
-          children: <Widget>[
-            Transform.rotate(
-              angle: ((qiblahDirection.direction ?? 0) * (math.pi / 180) * -1),
-              child: Container(
-                  color: Colors.green,
-                  child: SizedBox(
-                    height: 10,
-                    width: 20,
-                  )),
-            ),
-            Transform.rotate(
-              angle: ((qiblahDirection.qiblah ?? 0) * (math.pi / 180) * -1),
-              alignment: Alignment.center,
-              child: Container(
-                  color: Colors.red,
-                  child: SizedBox(
-                    height: 10,
-                    width: 20,
-                  )),
-            ),
-            Positioned(
-              bottom: 8,
-              child: Text("${qiblahDirection.offset.toStringAsFixed(3)}°"),
-            )
           ],
         );
       },
